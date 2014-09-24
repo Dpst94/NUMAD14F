cd0731f3254f5993b916647ff094379d6274cad0
Deborah Ho <ho.de@husky.neu.edu>
2014-09-24 14:37:56 -0400

    Started working on the dictionary more seriously. Split the wordlist in smaller text files.

---
+++ NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/dictionary/DictAcknowledgements.java
@@ -0,0 +1,15 @@
+package edu.neu.madcourse.deborahho.dictionary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import edu.neu.madcourse.deborahho.R;
+
+public class DictAcknowledgements extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dict_acknowledgements);
+		
+	}
+
+}
--- NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/dictionary/Dictionary.java
+++ NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/dictionary/Dictionary.java
@@ -1,24 +1,87 @@
 package edu.neu.madcourse.deborahho.dictionary;
 
 import edu.neu.madcourse.deborahho.R;
-import android.os.Bundle; // A mapping from String values to various Parcelable types.
-import android.app.Activity; // Required to create an activity.
-import android.view.Menu; // Interface for managing the items in a menu.
+import android.os.Bundle;
+import android.app.Activity; 
+import android.content.Intent;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu; 
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Dictionary extends Activity { // all classes extends activity
-        @Override
-        protected void onCreate(Bundle savedInstanceState) { // Create an activity/ screen
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.dictionary); // display activity textbox  when app starts (Main)
-        }
+import java.io.*;
+
+
+public class Dictionary extends Activity implements OnClickListener { 
         
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-                // Inflate the menu; this adds items to the action bar if it is present.
-        	super.onCreateOptionsMenu(menu);    
-        	MenuInflater inflater = getMenuInflater();
-            inflater.inflate(R.menu.main, menu);
-                return true;
-        }
+	EditText wordText;
+	TextView tv;
+
+	@Override    
+	protected void onCreate(Bundle savedInstanceState) { 
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dictionary); 
+        
+		View clear_button = findViewById(R.id.dict_clear_button);
+        clear_button.setOnClickListener(this);
+        View return_button = findViewById(R.id.dict_return_button);
+        return_button.setOnClickListener(this);
+        View acknowledgements_button = findViewById(R.id.dict_acknowledgements_button);
+        acknowledgements_button.setOnClickListener(this);
+
+        tv = (TextView) findViewById(R.id.found_words);
+        wordText = (EditText) findViewById(R.id.text_field);
+        wordText.addTextChangedListener(new TextWatcher () {
+        	
+        	@Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                // TODO Auto-generated method stub
+            }
+        	
+        	@Override
+        	public void afterTextChanged(Editable s) {
+        		String word = wordText.getText().toString();
+        		tv.append("\n"+word);
+        	}
+        });
+	}
+        
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+    
+		super.onCreateOptionsMenu(menu);    
+
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);      
+		return true;
+	}
+	
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.dict_clear_button:
+			break;
+		case R.id.dict_return_button:
+			finish();
+			break;
+		case R.id.dict_acknowledgements_button:
+			Intent i = new Intent(this, DictAcknowledgements.class);
+			startActivity(i);
+			break;
+		}
+	}
+	
+
 }
