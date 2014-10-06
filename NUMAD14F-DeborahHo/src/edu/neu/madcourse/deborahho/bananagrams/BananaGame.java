fe7b922e9f1e6421f248728a97fdc7a66f41570c
Deborah Ho <ho.de@husky.neu.edu>
2014-10-06 17:59:34 -0400

    Added a timer to game screen.

--- NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/bananagrams/BananaGame.java
+++ NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/bananagrams/BananaGame.java
@@ -3,13 +3,17 @@ package edu.neu.madcourse.deborahho.bananagrams;
 import edu.neu.madcourse.deborahho.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class BananaGame extends Activity implements OnClickListener {
 	private static final String TAG = "Bananagrams" ;
+	private CountDownTimer countDownTimer;
+	public TextView timerText; 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,23 @@ public class BananaGame extends Activity implements OnClickListener {
         pauseButton.setOnClickListener(this);
         View exitButton = findViewById(R.id.banana_quit_button);
         exitButton.setOnClickListener(this);
+        
+        timerText = (TextView) this.findViewById(R.id.banana_timer);
+        countDownTimer = new CountDownTimer(60000, 1000) {
+
+			@Override
+			public void onFinish() {
+				timerText.setText("Finished!");
+				
+			}
+
+			@Override
+			public void onTick(long millisUntilFinished) {
+				timerText.setText(" " + millisUntilFinished/1000);
+				
+			}
+        	
+        }.start();
     }
     
     @Override
@@ -38,11 +59,8 @@ public class BananaGame extends Activity implements OnClickListener {
     	case R.id.banana_quit_button:
     		finish();
     		break;
-
     	}
 		
 	}
-    
-
 
 }
