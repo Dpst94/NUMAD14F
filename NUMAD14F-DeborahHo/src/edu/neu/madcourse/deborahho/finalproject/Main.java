3c05b6671b33e4e903ca07571036854f2a30bbd9
Deborah Ho <ho.de@husky.neu.edu>
2014-11-21 14:02:44 -0500

    Two icons, start of the real Crunchy app.

---
+++ NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/finalproject/Main.java
@@ -0,0 +1,15 @@
+package edu.neu.madcourse.deborahho.finalproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import edu.neu.madcourse.deborahho.R;
+
+public class Main extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.finalproject_description);
+		
+	}
+
+}
