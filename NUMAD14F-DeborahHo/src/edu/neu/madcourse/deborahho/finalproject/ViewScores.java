cdc66c7a941bef2ea83db588592a9345bfcbafe1
eighilaza <elyes.ighilaza@gmail.com>
2014-12-03 18:03:58 -0500

    added score calculation

--- NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/finalproject/ViewScores.java
+++ NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/finalproject/ViewScores.java
@@ -1,7 +1,10 @@
 package edu.neu.madcourse.deborahho.finalproject;
 
+import java.util.Calendar;
+
 import edu.neu.madcourse.deborahho.R;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -9,16 +12,19 @@ import android.widget.TextView;
 
 public class ViewScores extends Activity implements OnClickListener{
 	
-	TextView day01;
+	TextView user01;
+	TextView score01;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.finalproject_view_scores);
 		
-		day01 = (TextView) findViewById(R.id.schedule_day_1);
+		user01 = (TextView) findViewById(R.id.username_01);		
+		user01.append("Debbie");
 		
-		day01.append("Day 01");
+		score01 = (TextView) findViewById(R.id.score_01);		
+		score01.append(""+calculateScore());
 
         View backButton = findViewById(R.id.finalproject_back_button);
         backButton.setOnClickListener(this);
@@ -31,5 +37,39 @@ public class ViewScores extends Activity implements OnClickListener{
 			finish();
 			break;
 		}
+	}
+	
+	private int calculateScore(){
+		int score = 0;
+		int today = 0;
+		int isDone = 1;
+		
+		Calendar c;
+		c = Calendar.getInstance();
+		SharedPreferences schedule = getSharedPreferences(WorkOutConstants.DAY_PREFS, 0);
+		int first_day = schedule.getInt("day_nb", 0);
+		if(first_day == 0){
+			today = 0;
+		}
+		else{
+			today = c.get(Calendar.DAY_OF_YEAR) - first_day;
+		}
+		
+		for(int i  = 0; i < today+1; i++){
+			SharedPreferences workout = getSharedPreferences(WorkOutConstants.DONE_WORKOUT_PREFS, 0);
+			isDone = workout.getInt(""+i, WorkOutConstants.DONE);
+			if(isDone==2){
+				score = score + 5;
+			}
+			else if(isDone==0){
+				score = score - 5;
+			}
+			else{
+				SharedPreferences.Editor editor = workout.edit();
+				editor.putInt(""+i, WorkOutConstants.MISS);
+				editor.commit();
+			}
+		}
+		return score;
 	}
 }
