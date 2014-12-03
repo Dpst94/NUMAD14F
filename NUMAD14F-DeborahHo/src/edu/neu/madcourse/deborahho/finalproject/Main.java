ba3ac7f9a913e0b6a1eac2acd6040e43e18a6027
eighilaza <elyes.ighilaza@gmail.com>
2014-12-03 12:14:29 -0500

    display tutorial only when app opened for first time

--- NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/finalproject/Main.java
+++ NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/finalproject/Main.java
@@ -2,33 +2,66 @@ package edu.neu.madcourse.deborahho.finalproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import edu.neu.madcourse.deborahho.R;
 
-public class Main extends Activity implements OnClickListener{
+public class Main extends Activity implements OnClickListener {
+
+	public static final String FIRST_OPEN_PREFS = "FirstOpen";
+	boolean never_opened = true;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setTitle(R.string.crunchy_label);
-		setContentView(R.layout.finalproject_main);
-		
-		View nextButton = findViewById(R.id.finalproject_next_button);
-        nextButton.setOnClickListener(this);
-		
+
+		// Check if the app has been used before
+		SharedPreferences first = getSharedPreferences(FIRST_OPEN_PREFS, 0);
+		never_opened = first.getBoolean("never_opened", true);
+
+		if (never_opened == true) {
+			setTitle(R.string.crunchy_label);
+			setContentView(R.layout.finalproject_main);
+
+			View nextButton = findViewById(R.id.finalproject_next_button);
+			nextButton.setOnClickListener(this);
+		} else {
+			setTitle(R.string.crunchy_label);
+			setContentView(R.layout.finalproject_main_bis);
+
+			View menuButton = findViewById(R.id.finalproject_menu_button);
+			menuButton.setOnClickListener(this);
+			View startButton = findViewById(R.id.finalproject_start_button);
+			startButton.setOnClickListener(this);
+			View backButton = findViewById(R.id.finalproject_back_button);
+			backButton.setOnClickListener(this);
+		}
+
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-    	case R.id.finalproject_next_button:
-    		Intent i = new Intent(this, TutorialFriends.class);//Game.class);
-    		startActivity(i);
-    		break;
+		case R.id.finalproject_next_button:
+			Intent i = new Intent(this, TutorialFriends.class);
+			startActivity(i);
+			break;
+
+		case R.id.finalproject_menu_button:
+			startActivity(new Intent(this, CrunchyMenu.class));
+			break;
+
+		case R.id.finalproject_start_button:
+			startActivity(new Intent(this, Game.class));
+			break;
+			
+		case R.id.finalproject_back_button:
+			finish();
+			break;
 		}
-    		
-		
+
 	}
 
 }
--- NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/finalproject/TutorialPictures.java
+++ NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/finalproject/TutorialPictures.java
@@ -2,12 +2,17 @@ package edu.neu.madcourse.deborahho.finalproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import edu.neu.madcourse.deborahho.R;
 
 public class TutorialPictures extends Activity implements OnClickListener{
+	
+	public static final String FIRST_OPEN_PREFS = "FirstOpen";
+	boolean never_opened = true;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +27,13 @@ public class TutorialPictures extends Activity implements OnClickListener{
 	public void onClick(View v) {
 		switch (v.getId()) {
     	case R.id.finalproject_next_button:
+    		// We need an Editor object to make preference
+			// changes.
+			SharedPreferences schedule = getSharedPreferences(
+					FIRST_OPEN_PREFS, 0);
+			SharedPreferences.Editor editor = schedule.edit();
+			editor.putBoolean("never_opened", false);
+			editor.commit();
     		Intent i = new Intent(this, Game.class);
     		startActivity(i);
     		break;
