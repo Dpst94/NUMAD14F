24b60a09998519b9ea8f869a69c822bb04ff6438
eighilaza <elyes.ighilaza@gmail.com>
2014-12-05 13:28:45 -0500

    "added camera to the Game, still need to add random capture"

--- NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/finalproject/Game.java
+++ NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/finalproject/Game.java
@@ -17,6 +17,27 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+
 public class Game extends Activity implements OnClickListener,
 		SensorEventListener {
 
@@ -37,11 +58,42 @@ public class Game extends Activity implements OnClickListener,
 	
 	int day_nb = 0;
 	Calendar c;
+	
+	private static final String TAG = "CamTestActivity";
+	Preview preview;
+	Button buttonClick;
+	Camera camera;
+	Activity act;
+	Context context;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
 		setContentView(R.layout.finalproject_game);
+		
+		context = this;
+		act = this;
+
+		preview = new Preview(this, (SurfaceView)findViewById(R.id.surfaceView));
+		preview.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		((FrameLayout) findViewById(R.id.layout)).addView(preview);
+		preview.setKeepScreenOn(true);
+
+		preview.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+			}
+		});
+
+		Toast.makeText(context, getString(R.string.take_photo_help), Toast.LENGTH_LONG).show();
+		
 
 		mCurrentDay = (TextView) findViewById(R.id.crunchy_current_day);
 		mNrOfCrunches = (TextView) findViewById(R.id.crunchy_nr_of_crunches);
@@ -114,7 +166,6 @@ public class Game extends Activity implements OnClickListener,
 			}
 
 		}
-
 	}
 
 	@Override
@@ -123,10 +174,26 @@ public class Game extends Activity implements OnClickListener,
 		if (currentSensor != null)
 			sensorManager.registerListener(this, currentSensor,
 					SensorManager.SENSOR_DELAY_FASTEST);
+		int numCams = Camera.getNumberOfCameras();
+		if(numCams > 0){
+			try{
+				camera = Camera.open(1);
+				camera.startPreview();
+				preview.setCamera(camera);
+			} catch (RuntimeException ex){
+				Toast.makeText(context, getString(R.string.camera_not_found), Toast.LENGTH_LONG).show();
+			}
+		}
 	}
 
 	@Override
 	public void onPause() {
+		if(camera != null) {
+			camera.stopPreview();
+			preview.setCamera(null);
+			camera.release();
+			camera = null;
+		}
 		super.onPause();
 		sensorManager.unregisterListener(this);
 	}
@@ -199,5 +266,75 @@ public class Game extends Activity implements OnClickListener,
 				}
 			}
 		}
+	}
+	private void resetCam() {
+		camera.startPreview();
+		preview.setCamera(camera);
+	}
+
+	private void refreshGallery(File file) {
+		Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+		mediaScanIntent.setData(Uri.fromFile(file));
+		sendBroadcast(mediaScanIntent);
+	}
+
+	ShutterCallback shutterCallback = new ShutterCallback() {
+		public void onShutter() {
+			//			 Log.d(TAG, "onShutter'd");
+		}
+	};
+
+	PictureCallback rawCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			//			 Log.d(TAG, "onPictureTaken - raw");
+		}
+	};
+
+	PictureCallback jpegCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			new SaveImageTask().execute(data);
+			resetCam();
+			Log.d(TAG, "onPictureTaken - jpeg");
+		}
+	};
+
+	private class SaveImageTask extends AsyncTask<byte[], Void, Void> {
+
+		@Override
+		protected Void doInBackground(byte[]... data) {
+			FileOutputStream outStream = null;
+
+			// Write to SD Card
+			try {
+				File sdCard = Environment.getExternalStorageDirectory();
+				File dir = new File (sdCard.getAbsolutePath() + "/crunchy");
+				dir.mkdirs();				
+
+				String fileName = String.format("%d.jpg", System.currentTimeMillis());
+				File outFile = new File(dir, fileName);
+
+				outStream = new FileOutputStream(outFile);
+				outStream.write(data[0]);
+				outStream.flush();
+				outStream.close();
+
+				Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length + " to " + outFile.getAbsolutePath());
+
+				refreshGallery(outFile);
+				
+				Uri uri = Uri.fromFile(outFile);
+				String imgpath = Uploader.getPath(context, uri);
+				Log.d("IMGPATH",imgpath);
+				Uploader.uploadFile(imgpath);
+				
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+			}
+			return null;
+		}
+
 	}
 }
