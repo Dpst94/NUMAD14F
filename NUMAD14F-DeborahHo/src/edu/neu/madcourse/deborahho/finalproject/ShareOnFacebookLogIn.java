8b941b76578355286ced3cb977a13256bdcd120d
Deborah Ho <ho.de@husky.neu.edu>
2014-12-07 01:25:40 -0500

    Changed facebook share page.

--- NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/finalproject/ShareOnFacebookLogIn.java
+++ NUMAD14F-DeborahHo/src/edu/neu/madcourse/deborahho/finalproject/ShareOnFacebookLogIn.java
@@ -13,8 +13,8 @@ public class ShareOnFacebookLogIn extends Activity implements OnClickListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.finalproject_share_fb_login);
 		
-		View logInButton = findViewById(R.id.login_facebook_button);
-        logInButton.setOnClickListener(this);
+//		View logInButton = findViewById(R.id.login_facebook_button);
+//        logInButton.setOnClickListener(this);
         View backButton = findViewById(R.id.finalproject_back_button);
         backButton.setOnClickListener(this);
 		
@@ -23,11 +23,11 @@ public class ShareOnFacebookLogIn extends Activity implements OnClickListener{
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-    	case R.id.login_facebook_button:
-    		//Pop up fb log in screen..
-    		Intent i = new Intent(this, ShareOnFacebookPostWall.class);//TutorialFriends.class);
-    		startActivity(i);
-    		break;
+//    	case R.id.login_facebook_button:
+//    		//Pop up fb log in screen..
+//    		Intent i = new Intent(this, ShareOnFacebookPostWall.class);//TutorialFriends.class);
+//    		startActivity(i);
+//    		break;
     	case R.id.finalproject_back_button:
     		finish();
     		break;
